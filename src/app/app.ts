9017ae8964b8715098a7074648dd77b972b1dc50
Diego Arruda <arrudadiego45@gmail.com>
2025-05-30 16:36:45 -0300

    commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { PageBody } from './components/page-body/page-body';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, PageBody],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/page-body/page-body.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Input } from '../input/input';
+
+@Component({
+  selector: 'app-page-body',
+  imports: [Input],
+  templateUrl: './page-body.html',
+  styleUrl: './page-body.css'
+})
+export class PageBody {
+
+}
