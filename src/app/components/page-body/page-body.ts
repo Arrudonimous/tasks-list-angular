35e1e639628914997058fb6abe68fd8313c69b55
Diego Arruda <arrudadiego45@gmail.com>
2025-06-08 12:55:42 -0300

    Feat: implemented empty tasks list

--- src/app/components/page-body/page-body.ts
+++ src/app/components/page-body/page-body.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Input } from '../input/input';
 import { Button } from '../button/button';
+import { TasksList } from '../tasks-list/tasks-list';
 
 @Component({
   selector: 'app-page-body',
-  imports: [Input, Button],
+  imports: [Input, Button, TasksList],
   templateUrl: './page-body.html',
   styleUrl: './page-body.css'
 })
